53f285c1e6d60129cb61a74e5bd4c8c1e5f5c8a3
gabriel.pereira <gasouzape@gmail.com>
2024-10-22 08:34:15 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,70 @@
+import { Header } from './components/Header.tsx';
+import { Post } from './components/Post.tsx';
+import { Sidebar } from './components/Sidebar.tsx';
+
+import './global.css';
+
+import styles from './App.module.css';
+
+interface Content {
+  type: 'paragraph' | 'link';  
+  content: string;
+}
+
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/gasouzape.png',
+      name: 'Gabriel Pereira',
+      role: 'Dev Backend'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ] as Content[],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/gasouzape.png',
+      name: 'Gabriel Souza',
+      role: 'Dev Frontend'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ] as Content[],
+    publishedAt: new Date('2022-05-10 20:00:00'),
+  },
+];
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+        {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,28 @@
+import styles from './Sidebar.module.css';
+import { Avatar } from './Avatar';
+
+import { PencilLine } from '@phosphor-icons/react'
+
+export function Sidebar(){
+    return (
+        <aside className={styles.sidebar}>
+            <img  
+                className={styles.cover}
+                src="https://images.unsplash.com/photo-1531297484001-80022131f5a1?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+            />
+            
+            <div className={styles.profile}>
+                <Avatar src="https://github.com/gasouzape.png" />
+                <strong>Gabriel Pereira</strong>
+                <span>Web Developer</span>
+            </div>
+            
+            <footer>
+                <a href="#">
+                    <PencilLine size={20}/>
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    );
+}
